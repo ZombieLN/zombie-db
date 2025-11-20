f11cbaeadb1acd82ed17b83b3213211fce8a5af1
ZombieLN <dilishagim@gmail.com>
2025-11-20 15:11:49 +0530

    Update testing.js

--- plugins/testing.js
+++ plugins/testing.js
@@ -110,9 +110,9 @@ async (conn, mek, m, {
     const uptime = await runtime(process.uptime());
 
     // Load external bot details
-    const details = (await axios.get('https://mv-visper-full-db.pages.dev/Main/main_var.json')).data;
+    const details = (await axios.get('https://raw.githubusercontent.com/ZombieLN/zombie-db/refs/heads/main/main_var.json')).data;
 
-        const MNG = `*🫟 VISPER MD - Bot Menu*
+        const MNG = `*🫟 ZOMBIE MD - Bot Menu*
 
 > *Uptime:* ${uptime}
 > *RAM Usage:* ${ramUsage}
@@ -141,7 +141,7 @@ async (conn, mek, m, {
                     sections: [
                         {
                             title: "SELECT MENU",
-                            highlight_label: "VISPER-MD",
+                            highlight_label: "ZOMBIE-MD",
                             rows: [
                                 {
                                     header: "1",
